5529c1909a814d5ceb93668fe8fc675769e72564
Zesky665 <zharkoc@gmail.com>
2023-04-14 12:47:26 +0200

    Fix az creds

--- prefect_setup.sh
+++ prefect_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 prefect cloud login --key $PREFECT_KEY --workspace $PREFECT_WORKSPACE
-az login --service-principal --username $AZURE_APP_ID --password $AZURE_SECRET_PASSWORD --tenant $AZURE_TENANT_ID
+az login --service-principal --username $AZURE_CLIENT_ID --password $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
 cd opt/flows
 python deploy_blocks.py
 python deploy_flows.py
